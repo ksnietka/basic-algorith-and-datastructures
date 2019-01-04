1c1572f5e34bb3aa53935bd251c6e37396373cfa
Krzysztof Śnietka <krzysztof.snietka@relayr.io>
2019-01-04 16:52:04 +0100

    Simple circularBuffer

---
+++ src/com/ksnietka/DataStructures/buffers/CircularBuffer.java
@@ -0,0 +1,34 @@
+package com.ksnietka.DataStructures.buffers;
+
+public class CircularBuffer <T> {
+    private T[] buffer;
+    private int readCursor = 0;
+    private int writeCursor = 0;
+
+    public CircularBuffer(int size) {
+        buffer = (T[]) new Object[size];
+    }
+
+    public boolean offer(T value) {
+        if (buffer[writeCursor] != null)
+            return false;
+        buffer[writeCursor] = value;
+        writeCursor = next(writeCursor);
+        return true;
+    }
+
+    public T poll () {
+        final T value = buffer[readCursor];
+        if(value != null) {
+            buffer[readCursor] = null;
+            readCursor = next(readCursor);
+
+        }
+        return value;
+    }
+
+    private int next(int index) {
+        return (index + 1) % buffer.length;
+    }
+
+}
